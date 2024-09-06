d389bc67fa42ef3cf5425965a29aa1673073dbfc
afzal-2004 <moa44468@gmail.com>
2024-09-07 00:10:37 +0530

    versel  Deploy Problem

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ContextProvider } from "./context/contextprovider.jsx";
+import { ContextProvider } from "./context/Contextprovider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
