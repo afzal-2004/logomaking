8e62dc38116689705061256431d6b0c6343d095d
afzal-2004 <moa44468@gmail.com>
2024-09-07 22:50:03 +0530

    Debug  for Deploayment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ContextProvider } from "./context/contextprovider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </StrictMode>
+  // <StrictMode>
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+  // </StrictMode>
 );
